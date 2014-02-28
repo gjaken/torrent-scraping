9e74eb2810e9d47bfa45ee945c1fe685a0e6e128
gjakenagel <gjaken47@gmail.com>
2014-02-28 12:42:52 -0800

    added useful links for functions

---
+++ useful_links.sh
@@ -0,0 +1,11 @@
+# lsof
+# http://www.thegeekstuff.com/2012/08/lsof-command-examples/
+# http://www.catonmat.net/blog/unix-utilities-lsof/
+
+# netstat
+# http://pcsupport.about.com/od/commandlinereference/p/netstat-command.htm
+# http://www.thegeekstuff.com/2010/03/netstat-command-examples/
+
+# fuser
+# http://www.thegeekstuff.com/2012/02/linux-fuser-command/
+# http://linux.101hacks.com/unix/fuser/
